1559ad0bc3574aacf3ff8b0dddf21c38b2145840
Anmol99914 <giria4621@gmail.com>
2026-05-25 16:29:20 +0545

    lab 9 done:)

--- Lab_Report_9_Constructors_Destructors/Program.cs
+++ Lab_Report_9_Constructors_Destructors/Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Student
+{
+    string name;
+    int age;
+
+    // Constructor
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Method to display data
+    public void Display()
+    {
+        Console.WriteLine("Student Name: " + name);
+        Console.WriteLine("Student Age: " + age);
+    }
+
+    // Destructor
+    ~Student()
+    {
+        Console.WriteLine("Destructor Called");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Object creation
+        Student s1 = new Student("Anmol", 20);
+
+        // Display object data
+        s1.Display();
+
+        Console.ReadLine();
+    }
+}
