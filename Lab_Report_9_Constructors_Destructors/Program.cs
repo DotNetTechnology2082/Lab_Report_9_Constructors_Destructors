8900cff637f89522143035cf2b230ffd0162aa27
Namrata <namratabca23@gmail.com>
2026-05-26 07:51:18 +0545

    this is lab 9

--- Lab_Report_9_Constructors_Destructors/Program.cs
+++ Lab_Report_9_Constructors_Destructors/Program.cs
@@ -2,19 +2,17 @@
 
 class Student
 {
-    // Data members
     string name;
     int age;
 
     // Constructor
     public Student(string name, int age)
     {
-        // Using this keyword
         this.name = name;
         this.age = age;
     }
 
-    // Method to display student details
+    // Method to display data
     public void Display()
     {
         Console.WriteLine("Student Name: " + name);
@@ -24,7 +22,7 @@ class Student
     // Destructor
     ~Student()
     {
-        Console.WriteLine("Destructor called.");
+        Console.WriteLine("Destructor Called");
     }
 }
 
@@ -32,13 +30,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Object creation
-        Student s1 = new Student("Neha", 21);
+        Student s1 = new Student("Namrata", 22);
 
-        // Display data
         s1.Display();
 
-        Console.WriteLine("Press Enter to exit...");
         Console.ReadLine();
     }
 }
