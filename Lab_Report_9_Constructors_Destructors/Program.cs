8a2d9145c1e67b492d6a554aaf378ce4b1ca85ae
Rahul24 <rahulbca23@oic.edu.np>
2026-06-06 19:02:34 +0545

    Lab 9 Jay Nepal

--- Lab_Report_9_Constructors_Destructors/Program.cs
+++ Lab_Report_9_Constructors_Destructors/Program.cs
@@ -33,7 +33,7 @@ class Program
     static void Main(string[] args)
     {
         // Object creation
-        Student s1 = new Student("Neha", 21);
+        Student s1 = new Student("Rahul", 22);
 
         // Display data
         s1.Display();
