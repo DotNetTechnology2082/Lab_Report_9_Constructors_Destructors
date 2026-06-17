8bc8f833dc73a6a197e13dd2dff5a78a0be1f3e5
Pasang_18 <pasang_lbca23@oic.edu.np>
2026-06-17 08:25:34 +0545

    Lab 9

--- Lab_Report_9_Constructors_Destructors/Program.cs
+++ Lab_Report_9_Constructors_Destructors/Program.cs
@@ -33,7 +33,7 @@ class Program
     static void Main(string[] args)
     {
         // Object creation
-        Student s1 = new Student("Neha", 21);
+        Student s1 = new Student("Pasang Lama", 21);
 
         // Display data
         s1.Display();
